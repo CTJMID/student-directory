fc23504e877cb91f67322b53bf57d4e01d8fb05d
Chris Middleton <ctjmiddleton@gmail.com>
2022-01-19 12:37:30 +0000

    Interactive Menu

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-# let's  put all students into an array
-students = [ 
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of The West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 
 def print_header
   puts "The students of Villains Academy"
@@ -47,8 +33,26 @@ def input_students
     students
 end
 
-#call methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit 
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
